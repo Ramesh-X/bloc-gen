eb537e472c91448ca9933d5e5a1f5d66ab01e87c
ramesh <ramesh@fcodelabs.com>
2020-03-21 20:41:48 +0530

    add to model

--- add_to_model.py
+++ add_to_model.py
@@ -11,23 +11,33 @@ def main():
 
     path = args[1]
     t = input("Enter variable Type (as String): ")
+    if type(t) is not str:
+        print("Invalid Variable Type")
+        sys.exit(1)
     v = input("Enter variable name (as myText): ")
-    i = 0
+    if type(t) is not str:
+        print("Invalid Variable Name")
+        sys.exit(1)
+
+    i = 1
     text = ''
     with open(path, 'r') as r_f:
         for line in r_f:
             text += line
-            if line.trim().endswith('{'):
-                i += 1
+            if line.strip().endswith('{'):
                 if i == 1:
-                    text += 'final %s %s;' % (t, v)
+                    i += 1
+                    text += 'final %s %s;\n' % (t, v)
                 elif i == 2:
-                    text += '@required this.%s,' %v
+                    i += 1
+                    text += '@required this.%s,\n' %v
                 elif i == 3:
-                    text += '%s %s,' % (t, v)
-            elif line.trim().endswith('(') and i == 3:
+                    i += 1
+                    text += '%s %s,\n' % (t, v)
+            elif line.strip().endswith('(') and i == 4:
                 i += 1
-                text += '%s: %s ?? this.%s,' % (v, v, v)
+                text += '%s: %s ?? this.%s,\n' % (v, v, v)
+
     with open(path, 'w') as w_f:
         w_f.write(text)
 
